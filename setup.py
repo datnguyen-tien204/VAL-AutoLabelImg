65c98816cbff9d786a1a9fd0ea0e95043644ee63
Dat Nguyen-Tien <118755598+datnguyen-tien204@users.noreply.github.com>
2024-09-18 19:49:33 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='VAL_LabelImgs',
-    version='0.1.6',
+    version='0.1.6rc0',
     packages=find_packages(),
     install_requires=[
         'opencv-python',
